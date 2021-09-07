797d37f1c305d51daaf816ba0ae17beb7ff90e13
cyrilvincent <contact@cyrilvincent.com>
2021-09-07 08:50:16 +0200

    SAGE

--- Arduino/04_06_sketch_potentio/04_06_sketch_potentio.ino
+++ Arduino/04_06_sketch_potentio/04_06_sketch_potentio.ino
@@ -31,12 +31,6 @@ void loop() {
     {
       digitalWrite(LED_BUILTIN, HIGH);
     }
-  int a = analogRead(pinTempSensor);
-  float R = 1023.0/a-1.0;
-  R = R0*R;
-  float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-  Serial.print("temperature = ");
-  Serial.println(temperature);
   delay(1000);
 }
 
